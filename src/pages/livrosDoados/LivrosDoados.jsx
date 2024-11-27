971405d70d5287dba7ca933faf7b21b509db7ec9
talli04 <tallitacris28@gmail.com>
2024-11-27 20:11:42 -0300

    adicionando sessão quero doar

--- src/pages/livrosDoados/LivrosDoados.jsx
+++ src/pages/livrosDoados/LivrosDoados.jsx
@@ -1,5 +1,16 @@
+
+import livro from '../../assets/livro.png'
+import s from './livrosDoados.module.scss'
 export default function LivrosDoados(){
     return(
-        <h2>EU SOU O CONPONENTE LIVROS DOADOS</h2>
+        <section className={s.livrosDoados}>
+            <h2>Livros Doados</h2>
+        <nav>
+            <a href=""><img src={livro} alt="" /></a>
+            <p>O protagonista</p>
+            <p>Susanne Andrade</p>
+            <p>Ficção</p>
+        </nav>
+      </section>
     )
 }
--- src/pages/queroDoar/QueroDoar.jsx
+++ src/pages/queroDoar/QueroDoar.jsx
@@ -1,5 +1,21 @@
+import livroform from '../../assets/livroform.png'
+import s from './queroDoar.module.scss'
+
 export default function QueroDoar(){
     return(
-        <h2>EU SOU O COMPONENTE QUERO DOAR</h2>
+        <section className={s.form}>
+            <p>Por favavor, preencha o formulário com suas informações e as informações do livro.</p>
+            <form action="">
+               <div>
+                <img src={livroform} alt="imagem de ícone de livro aberto azul." />
+                <h2>Informações do Livro</h2>
+                </div> 
+                <input type="text" name="" id="" placeholder='Titulo' />
+                <input type="text" name="" id="" placeholder='Categoria' />
+                <input type="text" name="" id="" placeholder='Autor' />
+                <input type="text" name="" id="" placeholder='Link da imagem' />
+                <input type="submit" value="Doar" className={s.buttonDoar} />
+            </form>
+        </section>
     )
 }
