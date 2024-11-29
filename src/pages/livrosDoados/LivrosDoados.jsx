c3c0aa3d2b6dab918f6bf9c10a2034767b704f52
Tallita Cristine <tallitacris28@gmail.com>
2024-11-29 14:52:27 -0300

    Atualizar o LivrosDoados.jsx

--- src/pages/livrosDoados/LivrosDoados.jsx
+++ src/pages/livrosDoados/LivrosDoados.jsx
@@ -3,9 +3,9 @@ import livro from '../../assets/livro.png'
 import s from './livrosDoados.module.scss'
 export default function LivrosDoados(){
     return(
-        <section>
-            <h2>Livros Doados</h2>
         <section classname={s.livrosDoadosSection}>
+            <h2>Livros Doados</h2>
+        <section classname={s.containerCards}>
             <section>
             <img src={livro} alt="imagem do livro o protagonista" />
             <h3>O protagonista</h3>
