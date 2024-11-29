e403fd48078f4289731d23a0d7412b7df357249e
Tallita Cristine <tallitacris28@gmail.com>
2024-11-29 14:43:34 -0300

    Atualizar o LivrosDoados.jsx

--- src/pages/livrosDoados/LivrosDoados.jsx
+++ src/pages/livrosDoados/LivrosDoados.jsx
@@ -5,12 +5,14 @@ export default function LivrosDoados(){
     return(
         <section className={s.livrosDoados}>
             <h2>Livros Doados</h2>
-        <nav>
-            <a href=""><img src={livro} alt="" /></a>
+        <section>
+            <section>
+            <img src={livro} alt="imagem do livro o protagonista" />
             <h3>O protagonista</h3>
             <p>Susanne Andrade</p>
             <p>Ficção</p>
-        </nav>
+            </section>
+        </section>
       </section>
     )
 }
