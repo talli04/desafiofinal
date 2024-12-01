1a4e41157f023321f28c6be6abb06ff449c47d36
talli04 <tallitacris28@gmail.com>
2024-12-01 11:38:53 -0300

    adicionando sessão livros doados e a responsividade da header

--- src/pages/livrosDoados/LivrosDoados.jsx
+++ src/pages/livrosDoados/LivrosDoados.jsx
@@ -1,16 +1,20 @@
-
-import livro from '../../assets/livro.png'
+import livroProtagonista from '../../assets/livroProtagonista.png'
 import s from './livrosDoados.module.scss'
+
 export default function LivrosDoados(){
     return(
-        <section className={s.livrosDoados}>
+        <section className={s.livrosDoadosSection}>
             <h2>Livros Doados</h2>
-        <nav>
-            <a href=""><img src={livro} alt="" /></a>
-            <p>O protagonista</p>
-            <p>Susanne Andrade</p>
-            <p>Ficção</p>
-        </nav>
-      </section>
+            <section className={s.containerCards}>
+                <section>
+                    <img src={livroProtagonista} alt="Imagem do livro O Protagonista" />
+                    <div>
+                        <h3>O protagonista</h3>
+                        <p>Susanne Andrade</p>
+                        <p>Ficção</p>
+                    </div>
+                </section>
+            </section>
+        </section>
     )
 }
